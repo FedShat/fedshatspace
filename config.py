f14be9e2fc24ebeb7307365bc8e0ee6598d8e21a
fox <ivan51673@gmail.com>
2020-06-27 02:35:04 +0300

    Config

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
+    WAKATIME_KEY = os.environ.get('WAKATIME_KEY', None)
